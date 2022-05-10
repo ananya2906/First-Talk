c4d569395ee35fa15f87ebab29fcffcf53bbee71
as359 <ananya.s.2906@gmail.com>
2022-05-10 17:02:22 +0530

    Full Detection Working

--- detection_Tutorial.py
+++ detection_Tutorial.py
@@ -1,3 +1,4 @@
+from turtle import bgcolor, color
 from pylab import *
 import cv2
 import numpy as np
@@ -13,6 +14,7 @@ import mediapipe as mp
 # globalvariable=[]
 mp_holistic = mp.solutions.holistic # Holistic model     
 mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
@@ -107,7 +109,7 @@ DATA_PATH= "MP_Data"
 actions = np.array(['hello', 'thanks', 'iloveyou'])
 
 # Thirty videos worth of data
-no_sequences = 5
+no_sequences = 15
 
 # Videos are going to be 30 frames in length
 sequence_length = 30
@@ -243,5 +245,143 @@ model.add(Dense(actions.shape[0], activation='softmax'))
 # actions[np.argmax(res)]
 
 model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
+model.fit(X_train, y_train, epochs=1500, callbacks=[tb_callback])
 model.summary()
+
+
+#=====================
+#Make Predictions
+#=====================
+res = model.predict(X_test)
+# actions[np.argmax(res[4])]
+# actions[np.argmax(y_test[4])]
+
+
+#===============
+#Save Weights
+#===============
+model.save('action.h5')
+# del model
+# model.load_weights('action.h5')
+
+
+#===============================================
+#Evaluation using confusion Matrix and Accuracy
+#===============================================
+from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
+yhat = model.predict(X_test)
+ytrue = np.argmax(y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
+
+
+#===============================================
+# Test in Real Time
+#===============================================
+# from scipy import stats
+
+# colors = [(245,117,16), (117,245,16), (16,117,245)]
+
+# # color = (255, 0, 0)
+# def prob_viz(res, actions, input_frame, colors):
+#     output_frame = input_frame.copy()
+#     for num, prob in enumerate(res):
+        
+#         print(colors[num])
+#         color =colors[num]
+        
+#         print(type(colors[num]))
+#         print('Hello')
+
+#         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), (245,117,16), -1)
+
+#         # cv2.rectangle(output_frame, int(0,60+num*40), int((prob*100), 90+num*40), col, 0) 
+       
+#         # cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num],-1) 
+#         print(type(output_frame))
+#         print('Hello 1')
+#         print(output_frame)
+       
+#         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)       
+    
+#     return output_frame
+
+
+
+# ============================================
+# colors = [(245,117,16), (117,245,16), (16,117,245)]
+# print(type(colors))
+
+# def prob_viz(res, actions, input_frame, colors):
+#     output_frame = input_frame.copy()
+#     for num, prob in enumerate(res):
+#         cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num].astype(bgcolor), -1)
+#         cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+#     return output_frame
+
+
+
+# plt.figure(figsize=(18,18))
+# plt.imshow(prob_viz(res, actions, image, colors))
+
+
+# 1. New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.5
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            print(actions[np.argmax(res)])
+            predictions.append(np.argmax(res))
+            
+        # #3. Viz logic
+        #     if np.unique(predictions[-10:])[0]==np.argmax(res): 
+        #         if res[np.argmax(res)] > threshold: 
+                    
+        #             if len(sentence) > 0: 
+        #                 if actions[np.argmax(res)] != sentence[-1]:
+        #                     sentence.append(actions[np.argmax(res)])
+        #             else:
+        #                 sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+                print(sentence)
+
+        #     # Viz probabilities
+        #     image = prob_viz(res, actions, image, colors)
+            
+        # cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        # cv2.putText(image, ' '.join(sentence), (3,30), 
+        #                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+
+        # Break gracefully
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
